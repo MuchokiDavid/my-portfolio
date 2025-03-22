f72c40c7c9bdde62b1a645b97fdc571ce997ce56
david muchoki <dmmuchoki7@gmail.com>
2025-03-22 21:51:44 +0300

    Update NavBars component links for improved clarity and consistency

--- src/components/utils/NavBars.jsx
+++ src/components/utils/NavBars.jsx
@@ -88,7 +88,7 @@ export default function NavBars() {
               </li>
               <li>
                 <a
-                  href="#about"
+                  href="#education"
                   className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   Education
@@ -104,10 +104,10 @@ export default function NavBars() {
               </li>
               <li>
                 <a
-                  href="#works"
+                  href="#experience"
                   className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
                 >
-                  Work
+                  Experience
                 </a>
               </li>
               <li>
