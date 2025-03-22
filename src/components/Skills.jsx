f69b87ce303d5384db07ce1d9ebccef121b67ec7
david muchoki <dmmuchoki7@gmail.com>
2025-03-23 00:57:19 +0300

    Update Skills component for improved layout and styling consistency

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -14,8 +14,8 @@ import { IoLogoFigma } from "react-icons/io5";
 
 export default function Skills() {
   return (
-    <div id="skills">
-      <div className="container px-5 py-10 mx-auto">
+    <div id="skills" className="mt-8">
+      <div className="container px-2 py-6 mx-auto">
         <div className="text-center mb-20">
           <h1 className="sm:text-4xl text-3xl font-medium title-font text-gray-300 mb-4">
             Skills
@@ -33,7 +33,7 @@ export default function Skills() {
         <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-2 place-items-center">
           {/* HTML */}
           <div className="p-2 sm:w-1/2 w-full">
-            <div className="bg-gray-800 rounded flex p-4 h-full items-center">
+            <div className="bg-gray-800 rounded flex p-4 h-full min-w-fit items-center">
               <FaHtml5 className="text-indigo-400 w-6 h-6 flex-shrink-0 mr-4" />
               <span className="title-font font-medium text-white">HTML</span>
             </div>
@@ -41,7 +41,7 @@ export default function Skills() {
 
           {/* CSS */}
           <div className="p-2 sm:w-1/2 w-full">
-            <div className="bg-gray-800 rounded flex p-4 h-full items-center">
+            <div className="bg-gray-800 rounded flex p-4 h-full min-w-fit items-center">
               <FaCss3Alt className="text-indigo-400 w-6 h-6 flex-shrink-0 mr-4" />
               <span className="title-font font-medium text-white">CSS</span>
             </div>
@@ -49,7 +49,7 @@ export default function Skills() {
 
           {/* JavaScript */}
           <div className="p-2 sm:w-1/2 w-full">
-            <div className="bg-gray-800 rounded flex p-4 h-full items-center w-fit"> 
+            <div className="bg-gray-800 rounded flex p-4 h-full min-w-fit items-center w-fit"> 
               <FaJs className="text-indigo-400 w-6 h-6 flex-shrink-0 mr-4" />
               <span className="title-font font-medium text-white">JavaScript</span>
             </div>
@@ -57,7 +57,7 @@ export default function Skills() {
 
           {/* React */}
           <div className="p-2 sm:w-1/2 w-full">
-            <div className="bg-gray-800 rounded flex p-4 h-full items-center w-fit">
+            <div className="bg-gray-800 rounded flex p-4 h-full min-w-fit items-center w-fit">
               <FaReact className="text-indigo-400 w-6 h-6 flex-shrink-0 mr-4" />
               <span className="title-font font-medium text-white">React</span>
             </div>
@@ -65,7 +65,7 @@ export default function Skills() {
 
           {/* Python */}
           <div className="p-2 sm:w-1/2 w-full">
-            <div className="bg-gray-800 rounded flex p-4 h-full items-center">
+            <div className="bg-gray-800 rounded flex p-4 h-full min-w-fit items-center">
               <FaPython className="text-indigo-400 w-6 h-6 flex-shrink-0 mr-4" />
               <span className="title-font font-medium text-white">Python</span>
             </div>
@@ -73,7 +73,7 @@ export default function Skills() {
 
           {/* Django */}
           <div className="p-2 sm:w-1/2 w-full">
-            <div className="bg-gray-800 rounded flex p-4 h-full items-center">
+            <div className="bg-gray-800 rounded flex p-4 h-full min-w-fit items-center">
               <SiDjango className="text-indigo-400 w-6 h-6 flex-shrink-0 mr-4" />
               <span className="title-font font-medium text-white">Django</span>
             </div>
@@ -81,7 +81,7 @@ export default function Skills() {
 
           {/* Flask */}
           <div className="p-2 sm:w-1/2 w-full">
-            <div className="bg-gray-800 rounded flex p-4 h-full items-center">
+            <div className="bg-gray-800 rounded flex p-4 h-full min-w-fit items-center">
               <SiFlask className="text-indigo-400 w-6 h-6 flex-shrink-0 mr-4" />
               <span className="title-font font-medium text-white">Flask</span>
             </div>
@@ -89,7 +89,7 @@ export default function Skills() {
 
           {/* Git */}
           <div className="p-2 sm:w-1/2 w-full">
-            <div className="bg-gray-800 rounded flex p-4 h-full items-center">
+            <div className="bg-gray-800 rounded flex p-4 h-full min-w-fit items-center">
               <FaGitAlt className="text-indigo-400 w-6 h-6 flex-shrink-0 mr-4" />
               <span className="title-font font-medium text-white">Git</span>
             </div>
@@ -97,7 +97,7 @@ export default function Skills() {
 
           {/* Linux */}
           <div className="p-2 sm:w-1/2 w-full">
-            <div className="bg-gray-800 rounded flex p-4 h-full items-center">
+            <div className="bg-gray-800 rounded flex p-4 h-full min-w-fit items-center">
               <FaLinux className="text-indigo-400 w-6 h-6 flex-shrink-0 mr-4" />
               <span className="title-font font-medium text-white">Linux</span>
             </div>
@@ -105,7 +105,7 @@ export default function Skills() {
 
           {/* cPanel */}
           <div className="p-2 sm:w-1/2 w-full">
-            <div className="bg-gray-800 rounded flex p-4 h-full items-center">
+            <div className="bg-gray-800 rounded flex p-4 h-full min-w-fit items-center">
               <SiCpanel className="text-indigo-400 w-6 h-6 flex-shrink-0 mr-4" />
               <span className="title-font font-medium text-white">cPanel</span>
             </div>
@@ -113,7 +113,7 @@ export default function Skills() {
 
           {/* PostgreSQL */}
           <div className="p-2 sm:w-1/2 w-full">
-            <div className="bg-gray-800 rounded flex p-4 h-full w-fit items-center">
+            <div className="bg-gray-800 rounded flex p-4 h-full min-w-fit items-center">
               <SiPostgresql className="text-indigo-400 w-6 h-6 flex-shrink-0 mr-4" />
               <span className="title-font font-medium text-white">PostgreSQL</span>
             </div>
@@ -121,7 +121,7 @@ export default function Skills() {
 
           {/* MySQL */}
           <div className="p-2 sm:w-1/2 w-full">
-            <div className="bg-gray-800 rounded flex p-4 h-full items-center">
+            <div className="bg-gray-800 rounded flex p-4 h-full min-w-fit items-center">
               <SiMysql className="text-indigo-400 w-6 h-6 flex-shrink-0 mr-4" />
               <span className="title-font font-medium text-white">MySQL</span>
             </div>
@@ -129,7 +129,7 @@ export default function Skills() {
 
           {/* Figma */}
           <div className="p-2 sm:w-1/2 w-full">
-            <div className="bg-gray-800 rounded flex p-4 h-full items-center">
+            <div className="bg-gray-800 rounded flex p-4 h-full min-w-fit items-center">
               <IoLogoFigma className="text-indigo-400 w-6 h-6 flex-shrink-0 mr-4" />
               <span className="title-font font-medium text-white">Figma</span>
             </div>
@@ -137,14 +137,14 @@ export default function Skills() {
 
           {/* npm */}
           <div className="p-2 sm:w-1/2 w-full">
-            <div className="bg-gray-800 rounded flex p-4 h-full items-center">
+            <div className="bg-gray-800 rounded flex p-4 h-full min-w-fit items-center">
               <SiPostman className="text-indigo-400 w-6 h-6 flex-shrink-0 mr-4" />
               <span className="title-font font-medium text-white">Postman</span>
             </div>
           </div>
 
           <div className="p-2 sm:w-1/2 w-full">
-            <div className="bg-gray-800 rounded flex p-4 h-full items-center">
+            <div className="bg-gray-800 rounded flex p-4 h-full min-w-fit items-center">
               <SiNpm className="text-indigo-400 w-6 h-6 flex-shrink-0 mr-4" />
               <span className="title-font font-medium text-white">NPM</span>
             </div>
