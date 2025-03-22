ba6519f6e88c89ec650341e3ead596538219243e
david muchoki <dmmuchoki7@gmail.com>
2025-03-23 00:57:11 +0300

    Refactor Education component for improved structure and styling

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -2,52 +2,65 @@ import React from "react";
 
 export default function Education() {
   return (
-    <div id="education">
-      <div className="container relative flex flex-col justify-between h-full max-w-6xl px-10 mx-auto xl:px-0 mt-5">
-        <h2 className="mb-1 text-3xl font-extrabold leading-tight text-gray-900">
-          Services
-        </h2>
-        <p className="mb-12 text-lg text-gray-500">
-          Here is a few of the awesome Services we provide.
-        </p>
+    // <div id="education" className="mb-8">
+      <div id="education" className="container relative flex flex-col justify-between h-full max-w-6xl px-10 mx-auto xl:px-0 mt-5">
+        <div className="text-center">
+          <h1 className="sm:text-4xl text-3xl font-medium title-font text-gray-300 mb-4">
+            Education
+          </h1>
+          <p className="mb-12 text-lg text-gray-300">
+            Here is a my educational background and some of the things I have
+            learned over the years.
+          </p>
+        </div>
         <div className="w-full">
           <div className="flex flex-col w-full mb-10 sm:flex-row">
-            <div className="w-full mb-10 sm:mb-0 sm:w-1/2">
-              <div className="relative h-full ml-0 mr-0 sm:mr-10">
-                <span className="absolute top-0 left-0 w-full h-full mt-1 ml-1 bg-indigo-500 rounded-lg"></span>
-                <div className="relative h-full p-5border-2 border-indigo-500 rounded-lg">
-                  <div className="flex items-center -mt-1">
-                    <h3 className="my-2 ml-3 text-lg font-bold text-gray-800">
-                      DAPP Development
+            <div className="w-full mb-10 sm:mb-0 sm:w-1/2 ">
+              <div className="relative h-full ml-0 mr-0 sm:mr-10 shadow-indigo-500 shadow-md hover:shadow-lg hover:scale-105 transition duration-300">
+                <span className="absolute top-0 left-0 w-full h-full mt-1 ml-1 rounded-lg"></span>
+                <div className="relative h-full p-5 border-2 border-indigo-500 rounded-lg">
+                  <div className="flex items-center -mt-1 gap-3">
+                    <h3 className="my-2 ml-3 text-lg font-bold text-indigo-500">
+                      Moringa School
                     </h3>
+                    <p className="ml-2 text-xs font-medium text-gray-50">
+                      2023 - 2024
+                    </p>
                   </div>
-                  <p className="mt-3 mb-1 text-xs font-medium text-indigo-500 uppercase">
+                  <p className="mt-3 mb-2 text-xs font-medium text-gray-100 uppercase">
+                    Software Engineering
+                  </p>
+                  <p className="mt-1 mb-1 text-xs font-medium text-indigo-500 uppercase">
                     ------------
                   </p>
-                  <p className="mb-2 text-gray-600">
-                    A decentralized application (dapp) is an application built
-                    on a decentralized network that combines a smart contract
-                    and a frontend user interface.
+                  <p className="mb-2 text-white">
+                    Gained experience in the software development lifecycle and
+                    learned the importance of Agile methodologies.
                   </p>
                 </div>
               </div>
             </div>
             <div className="w-full sm:w-1/2">
-              <div className="relative h-full ml-0 md:mr-10">
-                <span className="absolute top-0 left-0 w-full h-full mt-1 ml-1 bg-purple-500 rounded-lg"></span>
-                <div className="relative h-full p-5border-2 border-purple-500 rounded-lg">
+              <div className="relative h-full ml-0 md:mr-10 shadow-purple-500 shadow-md hover:shadow-lg hover:scale-105 transition duration-300">
+                <span className="absolute top-0 left-0 w-full h-full mt-1 ml-1 rounded-lg"></span>
+                <div className="relative h-full p-5 border-2 border-purple-500 rounded-lg">
                   <div className="flex items-center -mt-1">
-                    <h3 className="my-2 ml-3 text-lg font-bold text-gray-800">
-                      Web 3.0 Development
+                    <h3 className="my-2 ml-3 text-lg font-bold text-purple-500">
+                      Kibabii University
                     </h3>
+                    <p className="ml-2 text-xs font-medium text-gray-50">
+                      2014 - 2018
+                    </p>
                   </div>
+                  <p className="mt-3 mb-2 text-xs font-medium text-gray-100 uppercase">
+                    Computer Science
+                  </p>
                   <p className="mt-3 mb-1 text-xs font-medium text-purple-500 uppercase">
                     ------------
                   </p>
-                  <p className="mb-2 text-gray-600">
-                    Web 3.0 is the third generation of Internet services that
-                    will focus on understanding and analyzing data to provide a
-                    semantic web.
+                  <p className="mb-2 text-white">
+                    Studied computer science and learned about algorithms, data
+                    structures, networking and software engineering.
                   </p>
                 </div>
               </div>
@@ -55,61 +68,77 @@ export default function Education() {
           </div>
           <div className="flex flex-col w-full mb-5 sm:flex-row">
             <div className="w-full mb-10 sm:mb-0 sm:w-1/2">
-              <div className="relative h-full ml-0 mr-0 sm:mr-10">
-                <span className="absolute top-0 left-0 w-full h-full mt-1 ml-1 bg-blue-400 rounded-lg"></span>
-                <div className="relative h-full p-5border-2 border-blue-400 rounded-lg">
+              <div className="relative h-full ml-0 mr-0 sm:mr-10 shadow-blue-400 shadow-md hover:shadow-lg hover:scale-105 transition duration-300">
+                <span className="absolute top-0 left-0 w-full h-full mt-1 ml-1 rounded-lg"></span>
+                <div className="relative h-full p-5 border-2 border-blue-400 rounded-lg">
                   <div className="flex items-center -mt-1">
-                    <h3 className="my-2 ml-3 text-lg font-bold text-gray-800">
-                      Project Audit
+                    <h3 className="my-2 ml-3 text-lg font-bold text-blue-400">
+                      54 Collective VC
                     </h3>
+                    <p className="ml-2 text-xs font-medium text-gray-50">
+                      2024
+                    </p>
                   </div>
+                  <p className="mt-3 mb-2 text-xs font-medium text-gray-100 uppercase">
+                    Entrepreneurship
+                  </p>
                   <p className="mt-3 mb-1 text-xs font-medium text-blue-400 uppercase">
                     ------------
                   </p>
-                  <p className="mb-2 text-gray-600">
-                    A Project Audit is a formal review of a project, which is
-                    intended to assess the extent up to which project management
-                    standards are being upheld.
+                  <p className="mb-2 text-gray-50">
+                    Gained valuable knowledge in business modeling, customer
+                    research, prototyping, test user-ability and entrepreneurial
+                    experimentation.
                   </p>
                 </div>
               </div>
             </div>
             <div className="w-full mb-10 sm:mb-0 sm:w-1/2">
-              <div className="relative h-full ml-0 mr-0 sm:mr-10">
+              <div className="relative h-full ml-0 mr-0 sm:mr-10 shadow-yellow-400 shadow-md hover:shadow-lg hover:scale-105 transition duration-300">
                 <span className="absolute top-0 left-0 w-full h-full mt-1 ml rounded-lg"></span>
-                <div className="relative h-full p-5 border-2 rounded-lg">
+                <div className="relative h-full p-5 border-2 border-yellow-400 rounded-lg">
                   <div className="flex items-center -mt-1">
-                    <h3 className="my-2 ml-3 text-lg font-bold text-gray-800">
-                      Hacking / RE
+                    <h3 className="my-2 ml-3 text-lg font-bold text-yellow-400">
+                      Zetech University
                     </h3>
+                    <p className="ml-2 text-xs font-medium text-gray-50">
+                      2023
+                    </p>
                   </div>
-                  <p className="mt-3 mb-1 text-xs font-medium uppercase">
+                  <p className="mt-3 mb-2 text-xs font-medium text-gray-100 uppercase">
+                    Digital Marketing
+                  </p>
+                  <p className="mt-3 mb-1 text-xs text-yellow-400 font-medium uppercase">
                     ------------
                   </p>
-                  <p className="mb-2 text-gray-600">
-                    A security hacker is someone who explores methods for
-                    breaching defenses and exploiting weaknesses in a computer
-                    system or network.
+                  <p className="mb-2 text-white">
+                    Learned about digital marketing strategies, social media
+                    marketing, search engine optimization, content marketing and email marketing.
                   </p>
                 </div>
               </div>
             </div>
             <div className="w-full sm:w-1/2">
-              <div className="relative h-full ml-0 md:mr-10">
-                <span className="absolute top-0 left-0 w-full h-full mt-1 ml-1 bg-green-500 rounded-lg"></span>
-                <div className="relative h-full p-5border-2 border-green-500 rounded-lg">
+              <div className="relative h-full ml-0 md:mr-10 shadow-green-500 shadow-md hover:shadow-lg hover:scale-105 transition duration-300">
+                <span className="absolute top-0 left-0 w-full h-full mt-1 ml-1 rounded-lg"></span>
+                <div className="relative h-full p-5 border-2 border-green-500 rounded-lg">
                   <div className="flex items-center -mt-1">
-                    <h3 className="my-2 ml-3 text-lg font-bold text-gray-800">
-                      Bot/Script Development
+                    <h3 className="my-2 ml-3 text-lg font-bold text-green-500">
+                      Blest Computer College
                     </h3>
+                    <p className="ml-2 text-xs font-medium text-gray-50">
+                      2013
+                    </p>
                   </div>
+                  <p className="mt-3 mb-2 text-xs font-medium text-gray-100 uppercase">
+                    Computer Packages
+                  </p>
                   <p className="mt-3 mb-1 text-xs font-medium text-green-500 uppercase">
                     ------------
                   </p>
-                  <p className="mb-2 text-gray-600">
-                    Bot development frameworks were created as advanced software
-                    tools that eliminate a large amount of manual work and
-                    accelerate the development process.
+                  <p className="mb-2 text-gray-50">
+                    Learned about computer basics, Microsoft Office tools, internet
+                    browsing, printing, computer hardware and software and their maintenance.
                   </p>
                 </div>
               </div>
@@ -117,6 +146,6 @@ export default function Education() {
           </div>
         </div>
       </div>
-    </div>
+    // </div>
   );
 }
