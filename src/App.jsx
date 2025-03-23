d6bd0e85c1a4e4d02f087f1dfa1a6d1917ad5b8d
david muchoki <dmmuchoki7@gmail.com>
2025-03-23 13:56:43 +0300

    Refactor App component layout for improved structure and responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
-import React from 'react'
-import Hero from './components/Hero'
-import NavBars from './components/utils/NavBars'
-import Footer from './components/utils/Footer'
-import Work from './components/Work'
-import Projects from './components/Projects'
-import Contact from './components/Contact'
-import Skills from './components/Skills'
-import Education from './components/Education'
+import React from "react";
+import Hero from "./components/Hero";
+import NavBars from "./components/utils/NavBars";
+import Footer from "./components/utils/Footer";
+import Work from "./components/Work";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Skills from "./components/Skills";
+import Education from "./components/Education";
 
 export default function App() {
   return (
     <div>
-      <NavBars/>
-      <Hero/>
-      <Education/>
-      <Skills/>
-      <Work/>
-      <Projects/>
-      <Contact/>
-      <Footer/>
+      <NavBars />
+      <div className="container mx-auto max-w-8xl px-10">
+        <Hero />
+        <Education />
+        <Skills />
+        <Work />
+        <Projects />
+        <Contact />
+      </div>
+      <Footer />
     </div>
-  )
+  );
 }
