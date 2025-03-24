c8fcf0096ed4a42ca625b58b04c31dccdbabba0e
david muchoki <dmmuchoki7@gmail.com>
2025-03-24 13:54:15 +0300

    Update NavBars component: format code for better readability and restore Education link

--- src/components/utils/NavBars.jsx
+++ src/components/utils/NavBars.jsx
@@ -29,7 +29,9 @@ export default function NavBars() {
               className="text-white flex gap-0.5 bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0 dark:bg-purple-600 dark:hover:bg-purple-700 focus:outline-none dark:focus:ring-purple-800"
               rel="noopener noreferrer"
             >
-              <span><MdCloudDownload className="h-5 w-5"/></span>
+              <span>
+                <MdCloudDownload className="h-5 w-5" />
+              </span>
               Resume
             </a>
             <button
@@ -86,14 +88,7 @@ export default function NavBars() {
                   Home
                 </a>
               </li>
-              <li>
-                <a
-                  href="#education"
-                  className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  Education
-                </a>
-              </li>
+
               <li>
                 <a
                   href="#skills"
@@ -118,6 +113,16 @@ export default function NavBars() {
                   Projects
                 </a>
               </li>
+
+              <li>
+                <a
+                  href="#education"
+                  className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  Education
+                </a>
+              </li>
+
               <li>
                 <a
                   href="#contact"
