31fe394c5613b47f62add44a7f2fad0d4e52b871
David Muchoki <39527412+MuchokiDavid@users.noreply.github.com>
2026-06-12 06:24:25 +0300

    Bold symbols

--- src/components/utils/NavBars.jsx
+++ src/components/utils/NavBars.jsx
@@ -18,7 +18,7 @@ export default function NavBars() {
             <span className="self-center text-2xl font-bold whitespace-nowrap text-purple-700">
               {"< "}
             </span>
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+            <span className="self-center text-2xl font-bold whitespace-nowrap dark:text-white">
               {"Dave"}
             </span>
             <span className="self-center text-2xl font-bold whitespace-nowrap text-purple-700">
